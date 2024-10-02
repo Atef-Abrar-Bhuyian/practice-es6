0213fffdf2f6ca1ffbe2f8c9f4dc7e375d523991
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2024-10-03 03:16:46 +0600

    task2 complete

---
+++ prac2.js
@@ -0,0 +1,32 @@
+
+// task-2: console .log  output: Petersburg, Herry
+
+let students = {
+    2222: {
+        name: "Jack",
+        section: "C",
+        class: "IX",
+        address: {
+            "building no": 12,
+            "street": "St. Jonson",
+            "city": "Petersburg",
+            "country": "UK"
+        }
+    },
+    3333: {
+        name: "Herry",
+        section: "D",
+        class: "X",
+        address: {
+            "building no": 85,
+            "street": "DC road",
+            "city": "Kachukhet",
+            "country": "Bangladesh"
+        }
+    }
+}
+
+
+const peter = students[2222].address.city;
+const herry = students[3333].name;
+console.log(peter,',', herry);
